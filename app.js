956e39a2c209c1dcf65c4072c198adc97906382e
Marc Scattolin <marcscattolin@gmail.com>
2023-08-23 10:51:37 -0400

    speed up, make so it only shows departures in a limited window (default 90min)

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 "use strict"
 import { closeDb, importGtfs, openDb, getStops, getStoptimes, getCalendars, getTrips } from 'gtfs';
 import express from 'express';
-import {PORT, config} from './config.js';
-import { getDistance } from 'geolib';
+import { PORT, config, defaultRadius, defaultMinutesInFuture } from './config.js';
+import { getDistance, timeConversion } from 'geolib';
 
 const app = express();
 
@@ -29,6 +29,28 @@ function compareDepartureTimes(time1, time2) {
         if (time1Components[i] > time2Components[i]) {return 1;}
     }
     return 0;
+}
+
+function addTimes(time1, time2) {
+    time1 = String(time1);
+    time2 = String(time2);
+    var time1Components = [Number(time1.substring(0, 2)), Number(time1.substring(3, 5)), Number(time1.substring(6, 8))];
+    var time2Components = [Number(time2.substring(0, 2)), Number(time2.substring(3, 5)), Number(time2.substring(6, 8))];
+    for (let i = 0; i < 3; i++) {
+        time1Components[i] += time2Components[i];
+    }
+    if (time1Components[2] > 59) {
+        time1Components[1] += Math.floor(time1Components[2] / 60);
+        time1Components[2] %= 60;
+    }
+    if (time1Components[1] > 59) {
+        time1Components[0] += Math.floor(time1Components[1] / 60);
+        time1Components[1] %= 60;
+    }
+    // Does not cap the first value, since timestamps can go to 24:00 and 25:00 in gtfs
+    return ("0" + time1Components[0]).slice(-2)   + ":" + 
+    ("0" + time1Components[1]).slice(-2) + ":" + 
+    ("0" + time1Components[2]).slice(-2);
 }
 
 function nowString() {
@@ -109,55 +131,63 @@ function getClosestStoptime(stopTimes, closeStops, coords) {
 app.get("/transit", (request, response) => {
     try {
         const coords = {longitude: Number(request.query.longitude), latitude: Number(request.query.latitude)};
-        const radius = Number(request.query.radius);
+        const radius = Number(request.query.radius || defaultRadius);
+        const minutesInFuture = Number(request.query.minutesInFuture || defaultMinutesInFuture);
+        
         const stopsInRadius = getStopsNearUs(coords, radius);
-
+        const now = nowString();
+        const maxTime = addTimes(now, "00:" + minutesInFuture + ":00");
         const stopTimes = [];
+        
+        // Get all stop times for all stops (ie every time a bus stops at a stop near us)
         for (const stop_id in stopsInRadius) {
             if (Object.prototype.hasOwnProperty.call(stopsInRadius, stop_id)) {
                 // Only loop over the properties not in the prototype (ie only the stop ids added to it)
                 stopTimes.push(...getStoptimes({stop_id: stop_id}, ['stop_id', 'trip_id', 'arrival_time', 'departure_time', 'pickup_type']));
             }
         }
+        
+        // Filter to keep upcoming departures, that are within the max time
+        // (tested to be faster than filtering at the end)
+        const stopTimesInWindow = stopTimes.filter((stopTime) => {
+            return compareDepartureTimes(stopTime.departure_time, maxTime) <= 0 &&
+            compareDepartureTimes(stopTime.departure_time, now) >= 0;
+        });
 
         // Deduplicate stops (if one bus line stops at many stops in radius, only keep closest)
         // Sort by trip_id to keep easier, since duplicates will have same trip_id
         const deduplicatedStops = [];
-        stopTimes.sort((a, b) => {
+        stopTimesInWindow.sort((a, b) => {
             return a.trip_id.localeCompare(b.trip_id);
         });
 
-        for (let i = 0; i < stopTimes.length; i++) {
-            if (i === stopTimes.length - 1) {
-                deduplicatedStops.push(stopTimes[i]);
+        for (let i = 0; i < stopTimesInWindow.length; i++) {
+            if (i === stopTimesInWindow.length - 1) {
+                deduplicatedStops.push(stopTimesInWindow[i]);
                 break;
             }
 
-            if (stopTimes[i].trip_id === stopTimes[i + 1].trip_id) {
+            if (stopTimesInWindow[i].trip_id === stopTimesInWindow[i + 1].trip_id) {
                 let lastIndex = i;
-                for (; lastIndex < stopTimes.length - 1; lastIndex++) {
-                    try {
-                        if (stopTimes[lastIndex].trip_id !== stopTimes[lastIndex + 1].trip_id) {
-                            break;
-                        }
-                    } catch (e) {
-                        console.log("lol");
+                for (; lastIndex < stopTimesInWindow.length - 1; lastIndex++) {
+                    if (stopTimesInWindow[lastIndex].trip_id !== stopTimesInWindow[lastIndex + 1].trip_id) {
+                        break;
                     }
                 }
-                const duplicates = stopTimes.slice(i, lastIndex + 1);
+                const duplicates = stopTimesInWindow.slice(i, lastIndex + 1);
 
                 const closest = getClosestStoptime(duplicates, stopsInRadius, coords);
                 deduplicatedStops.push(closest);
                 i = lastIndex; // the i++ will increment it past the last index
             } else {
-                deduplicatedStops.push(stopTimes[i]);
+                deduplicatedStops.push(stopTimesInWindow[i]);
             }
         }
 
         const currentServiceIds = getCurrentServiceIds();
         const todayStoptimes = [];
 
-        // Get only stoptimes for today that allow pickup
+        // Get only stoptimes for today that allow pickup and add bus line
         for (const stop of deduplicatedStops) {
             if (stop.pickup_type !== 0) {
                 break;
@@ -169,23 +199,19 @@ app.get("/transit", (request, response) => {
                     if (currentServiceId === trip.service_id) {
                         stop.trip_headsign = trip.trip_headsign;
                         stop.wheelchair_accessible = trip.wheelchair_accessible;
+                        
                         todayStoptimes.push(stop);
                     }
                 }    
             }
         }
         
-        // Get ones after now
-        const now = nowString();
-        const upcomingTimes = todayStoptimes.filter((a) => compareDepartureTimes(a.departure_time, now) >= 0);
-                
         // Sort based on departure time
-        upcomingTimes.sort((a, b) => {
+        todayStoptimes.sort((a, b) => {
             return compareDepartureTimes(a.departure_time, b.departure_time);
         });
 
-
-        response.status(200).json({ stopTimes: upcomingTimes });
+        response.status(200).json({ stopTimes: todayStoptimes });
     } catch(error) {
         response.status(500).send(error.toString());
     }
