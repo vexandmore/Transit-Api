cea80483e4cfe6e185fed3a114750a4b8be47cee
Marc Scattolin <marcscattolin@gmail.com>
2024-04-02 20:08:26 -0400

    fix default config

--- example-config.js
+++ example-config.js
@@ -7,3 +7,7 @@ export const config = {
         {url: "https://www.stm.info/sites/default/files/gtfs/gtfs_stm.zip", exclude: ["shapes", "agency"]}
     ]
 };
+// Default radius in m
+export const defaultRadius = 300.0;
+// Default minutes in the future to query
+export const defaultMinutesInFuture = 90.0;
